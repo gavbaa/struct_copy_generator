260b560bf442c528c06c6711e62008a6c3207654
George Vilches <gvilches@jcasolutions.com>
2018-02-20 16:42:32 -0500

    Copier supports soft type conversions.

--- gen.go
+++ gen.go
@@ -38,10 +38,11 @@ func writeCopyFunc(t1 *types.Named, t2 *types.Named, outputPackageName string) (
 
 	// spew.Dump(t1.Obj().Pkg().Name())
 
-	out := fmt.Sprintf("package %s\n", outputPackageName)
-	out += fmt.Sprintf("import one \"%s\"\n", t1.Obj().Pkg().Path())
-	out += fmt.Sprintf("import two \"%s\"\n", t2.Obj().Pkg().Path())
-	out += fmt.Sprintf("func Copy%s%sTo%s%s(a one.%s, b two.%s) {\n",
+	imports := map[string]string{}
+	imports[t1.Obj().Pkg().Path()] = "one"
+	imports[t2.Obj().Pkg().Path()] = "two"
+
+	funcHeader := fmt.Sprintf("func Copy%s%sTo%s%s(a *one.%s, b *two.%s) {\n",
 		t1.Obj().Pkg().Name(),
 		t1.Obj().Name(),
 		t2.Obj().Pkg().Name(),
@@ -51,6 +52,9 @@ func writeCopyFunc(t1 *types.Named, t2 *types.Named, outputPackageName string) (
 	)
 
 	i := 0
+	tmpCounter := 1
+	funcTop := ""
+	fieldBody := ""
 	for i < s1.NumFields() {
 		f1 := s1.Field(i)
 		j := 0
@@ -58,13 +62,50 @@ func writeCopyFunc(t1 *types.Named, t2 *types.Named, outputPackageName string) (
 			f2 := s2.Field(j)
 			if strings.ToLower(f1.Name()) == strings.ToLower(f2.Name()) {
 				// Fields match
-				out += fmt.Sprintf("a.%s = b.%s\n", f1.Name(), f2.Name())
+
+				// Do soft type conversions.
+				// println("f1.Type", f1.Type().String(), f2.Type().String())
+				f1Type := f1.Type().String()
+				f2Type := f2.Type().String()
+				if f1Type == f2Type {
+					fieldBody += fmt.Sprintf("b.%s = a.%s\n", f2.Name(), f1.Name())
+				} else if f1Type == "int" && f2Type == "int64" {
+					fieldBody += fmt.Sprintf("b.%s = int64(a.%s)\n", f2.Name(), f1.Name())
+				} else if f1Type == "int64" && f2Type == "int" {
+					fieldBody += fmt.Sprintf("b.%s = int(a.%s)\n", f2.Name(), f1.Name())
+				} else if f1Type == "int" && f2Type == "string" {
+					imports["strconv"] = ""
+					fieldBody += fmt.Sprintf("b.%s = strconv.FormatInt(a.%s, 10)\n", f2.Name(), f1.Name())
+				} else if f1Type == "int64" && f2Type == "string" {
+					imports["strconv"] = ""
+					fieldBody += fmt.Sprintf("b.%s = strconv.FormatInt(a.%s, 10)\n", f2.Name(), f1.Name())
+				} else if f1Type == "time.Time" && f2Type == "*github.com/golang/protobuf/ptypes/timestamp.Timestamp" {
+					// TODO Error handling
+					imports["github.com/golang/protobuf/ptypes"] = ""
+					funcTop += fmt.Sprintf("tmp%d, _ := ptypes.TimestampProto(a.%s)\n", tmpCounter, f1.Name())
+					fieldBody += fmt.Sprintf("b.%s = tmp%d\n", f1.Name(), tmpCounter)
+					tmpCounter += 1
+				} else {
+					println(fmt.Sprintf("Couldn't find a reasonable type match: %s, %s = %s", f1.Name(), f1.Type().String(), f2.Type().String()))
+				}
 			}
 			j += 1
 		}
 		i += 1
 	}
+
+	importStr := ""
+	for importPath, importAlias := range imports {
+		importStr += fmt.Sprintf("import %s \"%s\"\n", importAlias, importPath)
+	}
+
+	out := fmt.Sprintf("package %s\n", outputPackageName)
+	out += importStr
+	out += funcHeader
+	out += funcTop
+	out += fieldBody
 	out += "}\n"
+
 	cleanOutput, err := gofmt(out)
 	if err != nil {
 		return "", errors.New("gofmt failed: " + err.Error() + ".  Pre-formatted output is as follows:\n\n" + out)
@@ -93,5 +134,5 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 	}
-	println(funcOut)
+	os.Stdout.WriteString(funcOut)
 }
